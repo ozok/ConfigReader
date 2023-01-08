51683358d7834550fb53509d94661e7d34fe42a1
okanozc <okan@cloudpeer.com>
2023-01-08 14:26:15 +0300

    add: model attributes

--- Models/Models/Configuration.cs
+++ Models/Models/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,16 @@ namespace Models.Models
 {
     public class Configuration : BaseEntity
     {
+        [Required]
+        [StringLength(128)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(128)]
         public string Value { get; set; }
+
+        [Required]
+        [StringLength(128)]
         public string ApplicationName { get; set; }
     }
 }
